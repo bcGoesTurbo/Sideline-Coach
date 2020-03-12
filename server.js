677dd7ebe41edbc681e20755b2c806e6d4b8a4a9
brad <bcmonster@hotmail.com>
2020-03-12 19:22:55 +0800

    make it better

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require("mongoose");
+const path = require('path');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
-// }
+//Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/SidelineCoach",
     {
